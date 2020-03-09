b9a0f91fbfc479d4581daaaa875db8b1aaf39265
Onur Com <comkael@gmail.com>
2020-03-09 23:24:55 +0100

    Initial Commit

---
+++ WatchuGonnaWatchSUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchuGonnaWatchSUI
+//
+//  Created by Onur Com on 9.03.2020.
+//  Copyright © 2020 Onur Com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
